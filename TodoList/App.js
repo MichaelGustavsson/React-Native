80ccbd576dda4be34b68b6989ac4138688cb5c88
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-08-17 14:47:10 +0200

    Added ScrollView

--- TodoList/App.js
+++ TodoList/App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import uuid from 'react-native-uuid';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
+  const [todoText, setTodoText] = useState('');
+  const [todoItems, setTodoItems] = useState([]);
+
+  function onTextChangedHandler(text) {
+    setTodoText(text);
+  }
+
+  function onAddTodoHandler() {
+    setTodoItems((currentTodoItems) => [
+      ...currentTodoItems,
+      { text: todoText, key: uuid.v4() },
+    ]);
+  }
+
   return (
     <View style={styles.appContainer}>
-      <TextInput style={styles.textInput} placeholder='Lägg till att göra' />
-      <Button title='Lägg till' />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={onTextChangedHandler}
+          placeholder='Lägg till att göra'
+        />
+        <Button title='Lägg till' onPress={onAddTodoHandler} />
+      </View>
+      <View style={styles.todoListContainer}>
+        <ScrollView>
+          {todoItems.map((todoItem) => (
+            <Text key={todoItem.key} style={styles.todoItemContainer}>
+              {todoItem.text}
+            </Text>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -17,10 +54,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#f4f4f4',
   },
   textInput: {
+    flex: 1,
     borderWidth: 1,
-    borderColor: '#333',
+    borderColor: '#333333',
     padding: 4,
     paddingLeft: 8,
     marginBottom: 8,
+    marginRight: 8,
+  },
+
+  todoListContainer: {
+    borderTopColor: '#333333',
+    borderTopWidth: 1,
+    marginTop: 16,
+    paddingTop: 24,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  todoItemContainer: {
+    backgroundColor: '#333333',
+    color: '#ffffff',
+    padding: 8,
+    marginTop: 16,
   },
 });
